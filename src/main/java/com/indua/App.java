ea5d509e362fadaa0931b2e8fd8fae0a5b36fca4
Pavan Kishore <pavankishore2203@gmail.com>
2023-03-07 20:14:47 +0530

    test commit

--- src/main/java/com/indua/App.java
+++ src/main/java/com/indua/App.java
@@ -7,10 +7,6 @@ public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
